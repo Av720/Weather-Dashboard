323d1d0bca6bc31a57cbad542634a62165348452
CWhite2127 <cwhitewesterly2@gmail.com>
2023-02-22 12:04:41 -0700

    Declared global variables for start of JS

---
+++ script.js
@@ -0,0 +1,11 @@
+var searchIdEL = document.queryselector("#search-id");
+var searchBtnEl = document.querySelector("#searchBtn");
+var clearBtnEl = document.querySelector("#clear-button");
+var cityInfoEl = document.querySelector("#city-info");
+var temperatureEl = document.querySelector("#temperature");
+var humidityEl = document.querySelector("#humidity");
+var windEl = document.querySelector("#wind");
+var uvIndexEl = document.querySelector("#uv-index");
+var requestUrl = "https://api.openweathermap.org/data/2.5/forecast?";
+
+
