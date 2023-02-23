dc3581b91a095827b42bc739f0683393041798cc
CWhite2127 <cwhitewesterly2@gmail.com>
2023-02-23 10:21:45 -0700

    switching branches

--- script.js
+++ script.js
@@ -8,7 +8,10 @@ var windEl = document.querySelector("#wind");
 var uvIndexEl = document.querySelector("#uv-index");
 var requestUrl = "https://api.openweathermap.org/data/2.5/forecast?";
 
-
+function searchApi(searchIdEl) {
+  if (!searchIdEl) {
+    return;
+  }
   var cityLatEl = $(this).attr("searchIdEl").q.lat;
   var cityLonEl = $(this).attr("searchIdEl").q.lon;
   var apiKey = "2efa42872fdfe44cc2d10f0d272593c5";
