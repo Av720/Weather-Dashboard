180e37acefe2dae465544b7b7028fd7321790688
Av720 <120581801+Av720@users.noreply.github.com>
2023-02-23 11:28:10 -0700

    new pull

--- script.js
+++ script.js
@@ -8,21 +8,23 @@ var uvIndexEl = document.querySelector("#uv-index");
 var apiKey = "2efa42872fdfe44cc2d10f0d272593c5";
 
 function getCurrentWeather(searchInput) {
-  var searchInput = document.querySelector("#search-id").value;
-  var requestCurrentURL =
-    "https://api.openweathermap.org/data/2.5/weather?q=" +
-    searchInput +
-    "&appid=" +
-    apiKey +
-    "&units=imperial";
-  fetch(requestCurrentURL)
-    .then(function (response) {
-      console.log(response);
-      return response.json();
-    })
-    .then(function (data) {
-      console.log(data);
-    });
+    var searchInput = document.querySelector("#search-id").value;
+    var requestCurrentURL =
+        "https://api.openweathermap.org/data/2.5/weather?q=" +
+        searchInput +
+        "&appid=" +
+        apiKey +
+        "&units=imperial";
+    fetch(requestCurrentURL)
+        .then(function (response) {
+            console.log(response);
+            return response.json();
+        })
+        .then(function (data) {
+            console.log(data);
+        });
 }
 
 searchBtnEl.addEventListener("click", getCurrentWeather);
+
+
